c45cf1b8bc73bc52d7aa6f7c87129a337634077a
AliMehmetCetinkol <73673226+AliMehmetCetinkol@users.noreply.github.com>
2023-05-25 15:05:22 +0300

    Update SumMaxMinAvg.java

--- src/com/ali/homework4/SumMaxMinAvg.java
+++ src/com/ali/homework4/SumMaxMinAvg.java
@@ -3,7 +3,7 @@ package com.ali.homework4;
 import java.util.Scanner;
 
 public class SumMaxMinAvg {
-    public static void main(String[] args) {
+    public static void run() {
         UtilS.run();
     }
 }

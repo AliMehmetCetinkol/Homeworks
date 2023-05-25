3e90aaab76f8d999be703068a338a289c17783d3
AliMehmetCetinkol <73673226+AliMehmetCetinkol@users.noreply.github.com>
2023-05-25 14:47:51 +0300

    Update PrimeX.java

--- src/com/ali/homework3/PrimeX.java
+++ src/com/ali/homework3/PrimeX.java
@@ -5,7 +5,7 @@ In this method, it tests whether the argument sent is prime, and if the number i
 this time it checks whether the number obtained by adding the digits of the number is prime.
  */
 public class PrimeX {
-    public static void main(String[] args) {
+    public static void run() {
         run();
     }
 

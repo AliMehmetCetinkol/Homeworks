9da6c943c24996988d4dcbfb83957a61dd03c203
AliMehmetCetinkol <73673226+AliMehmetCetinkol@users.noreply.github.com>
2023-05-25 15:03:46 +0300

    Update GoldbachTheorem.java

--- src/com/ali/homework2/GoldbachTheorem.java
+++ src/com/ali/homework2/GoldbachTheorem.java
@@ -18,7 +18,7 @@ public class GoldbachTheorem {
         }
         return count == 2;
     }
-    public static void main(String[] args){
+    public static void run(){
         Scanner in = new Scanner(System.in);
         System.out.print("Enter a number : ");
         int n = in.nextInt();
